acec364266ff90374b3cfb272dc600a53ca805dc
Walt Ostrander <waltostrander@gmail.com>
2023-06-15 10:02:37 -0500

    set up server, created models and ran app to create all tables in db.

--- app.py
+++ app.py
@@ -1,9 +1,62 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/trail_management'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    projects = db.relationship('Project', backref='user', lazy=True)
+
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(64), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    tasks = db.relationship('Task', backref='project', lazy=True)
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(64), nullable=False)
+
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
+
+
+class Equipment(db.Model):
+    __tablename__ = 'equipment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(64), nullable=False)
+    maintenance_schedule = db.Column(db.String(128), nullable=False)
+
 
 @app.route('/')
-def home():
-    return "Hello, World!"
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    db.create_all()  # Create all tables
     app.run(debug=True)
