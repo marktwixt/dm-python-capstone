e7ee6dccecdf8224168ddd9aa8dd54fa9bb1538e
Walt Ostrander <waltostrander@gmail.com>
2023-06-15 15:48:53 -0500

    Refactor: Separated app and login manager initialization from models.py
    - Moved Flask app and LoginManager initialization to app.py.
    - Removed unnecessary imports from models.py.
    - Added user_loader function to app.py.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_login import LoginManager
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/trail_management'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- models.py
+++ models.py
@@ -2,15 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/trail_management'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -77,7 +69,3 @@ class ExpenseReport(db.Model):
     status = db.Column(db.String(64))  # Adjust as needed
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
